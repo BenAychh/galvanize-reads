946223bb25a8d793c49aace0a970f9cc42aee9ca
BenAychh <ben.tomas.hernandez@gmail.com>
2016-03-28 14:35:58 -0600

    backend support for index.html

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var knex = require('knex');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
